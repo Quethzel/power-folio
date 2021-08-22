cbe8a33df100ed76e218966ba623a1e4049ca3a4
Quethzel Diaz <quethzel@gmail.com>
2021-08-22 17:38:06 -0500

    add toggle theme button. import custom theme from layout.scss to styles.scss add olverlaycontainer to toolbar component

--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,16 +1,32 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Input() isDarkTheme: boolean = false;
   @Output() public sidenavToggle = new EventEmitter();
   @Output() public changeTheme = new EventEmitter<boolean>();
-  
-  constructor() { }
+  themeClass: string = 'theme-dark';
+
+  constructor(private overlayContainer: OverlayContainer) { }
+
+  ngOnInit() {
+    this.overlayContainer.getContainerElement().classList.add(this.themeClass);
+  }
+
+  onThemeChange(theme:string) {
+    this.themeClass = theme;
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.includes('-theme'));
+    if (themeClassesToRemove.length) {
+       overlayContainerClasses.remove(...themeClassesToRemove);
+    }
+    overlayContainerClasses.add(theme);
+  }
 
   themeToggle() {
     this.isDarkTheme = !this.isDarkTheme;
